6b2527a46299c94d339db547e4e7256be52b275c
Pellegrini Pierre <pellegrini.pierre@gmail.com>
2015-01-29 07:45:55 +0100

    Update# 10

--- src/Contents/Services/sql/reqPDF.sql
+++ src/Contents/Services/sql/reqPDF.sql
@@ -6,8 +6,8 @@ SELECT *
 FROM appelsoffres 
 inner join fichiers
 on appelsoffres.IdAppelOffre = fichiers.IdAO
-where IdAppelOffre = {pdf}
-/*where IdAppelOffre = 8883*/
+/*where IdAppelOffre = {pdf}*/
+where IdAppelOffre = 8883
 /*where appelsoffres.id_domaine = 2
 			&& appelsoffres.IdThematique = 4
 			&& IdNaturePrestation = 2
