6aa9aeebe72664a737b93c1fa84d54e3965848db
Pellegrini Pierre <pellegrini.pierre@gmail.com>
2015-01-29 07:23:19 +0100

    Update# 5

---
+++ src/Contents/Services/sql/reqPDF.sql
@@ -0,0 +1,12 @@
+/*
+requete de gestion appelsoffres
+*/
+
+SELECT *
+FROM appelsoffres 
+	/*where IdAppelOffre = {appeloffre}*/
+where IdAppelOffre = 8883
+/*where appelsoffres.id_domaine = 2
+			&& appelsoffres.IdThematique = 4
+			&& IdNaturePrestation = 2
+			&& IdDepartement = 3*/
